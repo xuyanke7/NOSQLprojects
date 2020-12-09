383268376f1872c5109c130b753984548d959aeb
xuyanke7 <42867860+xuyanke7@users.noreply.github.com>
2020-12-09 18:55:13 +0800

    Add files via upload

---
+++ problemLecture8/src/com/lecture8/Rank.java
@@ -0,0 +1,47 @@
+package com.lecture8;
+
+import java.util.Arrays;
+
+/**
+ * @author xyf09
+ */
+public class Rank {
+    public static String[] reRank(String[] input){
+        return new String[0];
+    }
+
+    public static String[] reRankTwoPointers(String[] input){
+        if(input==null||input.length==0){
+            return new String[0];
+        }
+        int i=0;
+        int left=0;
+        int right = input.length-1;
+        while(i < right){
+            if(input[i].equals("r")){
+                String t1 = input[i];
+                input[i]= input[left];
+                input[left]= t1;
+                i++;
+                left++;
+            }
+            else if(input[i].equals("g")){
+                i++;
+            }
+            else if(input[i].equals("b")){
+                String t2 = input[i];
+                input[i]= input[right];
+                input[right]= t2;
+                right--;
+            }
+        }
+        return input;
+    }
+
+
+
+    public static void main(String[] args) {
+        String[] input = new String[]{"r","r","b","r","b","g","g"};
+        Arrays.asList(reRankTwoPointers(input)).forEach(e -> System.out.println(e));
+    }
+}
